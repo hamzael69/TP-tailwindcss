0f122c7b4ded82a348a650d0e662b766bf9f329c
hamzael69 <elomarihamza835@gmail.com>
2024-10-30 16:59:34 +0100

    Deux premier section et fai re les hr

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          orange: 'hsl(35, 77%, 62%)',
+          red: 'hsl(5, 85%, 63%)',
+        },
+        neutral: {
+          'off-white': 'hsl(36, 100%, 99%)',
+          'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+          'dark-grayish-blue': 'hsl(236, 13%, 42%)',
+          'very-dark-blue': 'hsl(240, 100%, 5%)',
+        },
+      },
+      fontFamily: {
+        body: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        paragraph: '20px',
+      },
+    },
   },
   plugins: [],
 }
